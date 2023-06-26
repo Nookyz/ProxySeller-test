f77e82cbf93c6cd9bbe879a56a4343ce71b12bde
Pavlo Motrenko <pavlomotrenko222@gmail.com>
2023-06-26 11:05:12 +0300

    small refactoring

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import countersSlice from './slices/counters/focusCounter';
-// import shortBreakCounterSlice from './slices/counters/shortBreakCounter';
-// import longBreakCounterSlice from './slices/counters/longBreakCounter';
-// import counterStateSlice from './slices/state';
 import { jsonplaceholderApi } from './jsonplaceholder/jsonplaceholder';
 
 export const store = configureStore({
   reducer: {
-    // focusCounter: countersSlice,
-    // shortBreakCounter: shortBreakCounterSlice,
-    // longBreakCounter: longBreakCounterSlice,
-    // state: counterStateSlice,
     [jsonplaceholderApi.reducerPath]: jsonplaceholderApi.reducer
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(jsonplaceholderApi.middleware)
